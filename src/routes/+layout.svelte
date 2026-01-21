da57473975ca2b2949ed598b7b465bcc5a75966e
fabio <fabio.sales@me.com>
2026-01-21 19:09:46 -0300

    Data layer updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
+  import { afterNavigate } from '$app/navigation';
   import '../app.css';
+
+  afterNavigate((nav) => {
+    const url = nav?.to?.url;
+    if (!url || typeof window === 'undefined') return;
+    const path = `${url.pathname}${url.search}${url.hash}`;
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'page_view',
+      page_location: url.href,
+      page_path: path,
+      page_title: document.title
+    });
+  });
 </script>
 
 <svelte:head>
